0aaf9a5b3d92dde1ef62178ddb6798f876d207a0
masterishaan19 <45001655+masterishaan19@users.noreply.github.com>
2019-10-14 09:47:57 +0530

    This contain few more sorting algo for CP

---
+++ Merge_sort.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+void merge(int arr[], int start, int mid, int end){
+	int len = end -start +1;
+	int temp[len];
+	int i = start;
+	int j = mid+1;
+	int k = 0;
+	while(i <= mid && j<= end){
+		if(arr[i] <= arr[j])
+			temp[k++] = arr[i++];
+		else
+			temp[k++] = arr[j++];
+	}
+	while(i <= mid){
+		temp[k++] = arr[i++];
+	}
+	while(j <= end){
+		temp[k++] = arr[j++];
+	}
+	k =0;
+	for(int i=start; i<=end; i++){
+		arr[i] = temp[k++];
+	}
+}
+void msort(int arr[], int start, int end)
+{
+	if(start < end)
+	{
+		int mid = (start + end)/2;
+		msort(arr, start, mid);
+		msort(arr, mid+1, end);
+		merge(arr, start, mid, end);
+	}
+}
+int main()
+{
+	int num;
+	printf("Enter number of terms:");
+	scanf("%d",&num);
+	int arr[num];
+	for(int i=0; i<num; i++)
+		scanf("%d",&arr[i]);
+	int start = 0;
+	int end = num-1;
+	msort(arr, start, end);
+	printf("Your sorted array is:");
+	for(int i=0; i<num; i++)
+		printf("%d ",arr[i]);
+}
